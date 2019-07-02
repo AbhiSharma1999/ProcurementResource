13d334fe7b87e433704c7f80371c23107bc84b76
ProcrastinatorPro <abhishek3ic@gmail.com>
2019-07-02 15:11:34 +0530

    Added navigation click , Profile , Back button on toolbar ,Removed menu

--- app/src/main/java/com/example/abhishek/pr/MainActivity.kt
+++ app/src/main/java/com/example/abhishek/pr/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.abhishek.pr
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
@@ -35,6 +36,9 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
         val navigationView = findViewById<View>(R.id.nav_view) as NavigationView
         navigationView.setNavigationItemSelectedListener(this)
+
+        val headerview = navigationView.getHeaderView(0)
+        headerview.setOnClickListener { startActivity(Intent(this@MainActivity , ProfileActivity::class.java)) }
     }
 
     override fun onBackPressed() {
@@ -46,42 +50,20 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        val id = item.itemId
-
-
-        return if (id == R.id.action_settings) {
-            true
-        } else super.onOptionsItemSelected(item)
-
-    }
-
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Handle navigation view item clicks here.
         val id = item.itemId
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        when(id)
+        {
+            R.id.nav_send -> {}
+            R.id.nav_share -> {}
+            R.id.nav_manage -> {}
+            R.id.nav_gallery -> {}
+            R.id.nav_camera -> {}
+            R.id.nav_slideshow -> {}
         }
+
 
         val drawer = findViewById<View>(R.id.drawer_layout) as DrawerLayout
         drawer.closeDrawer(GravityCompat.START)
---
+++ app/src/main/java/com/example/abhishek/pr/ProfileActivity.kt
@@ -0,0 +1,16 @@
+package com.example.abhishek.pr
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+
+        supportActionBar!!.title = "Go Back"
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+    }
+}
